c97f7abe146b91281ac1e8a7e64a1daea25f7811
Mustafa Emir ATA <148717139+mustafaemirata@users.noreply.github.com>
2024-11-20 15:45:16 +0300

    Add files via upload

---
+++ MetinBelgesi/MetinBelgesi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace consoleApp
+
+{
+    class MetinBelgesi
+    {
+        static void Main(string[] args)
+        {
+            string filePath;
+            // C:\Kullanıcılar\Emir\Masaüstü\metinn.txt
+            filePath= @"C:\Users\Emir\Desktop\DENEMELER\yeniMetin2.txt";
+            //FileStream fs = new FileStream(filePath,FileMode.Create);
+
+            FileStream filestream = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(filestream);
+            writer.WriteLine("1.satir...");
+            writer.WriteLine("2.satir...");
+            writer.WriteLine("3.satir...");
+
+            writer.Flush();
+            writer.Close();
+            filestream.Close();
+            Console.WriteLine("Dosya yazdirma islemi başarıyla gerçekleşti.");
+
+
+        }
+    }
+}
